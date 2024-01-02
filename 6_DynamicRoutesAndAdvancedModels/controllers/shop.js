8c17545ded9ae2551b30d1fab0a896fda02a1d91
LanderLluv <landergll99@gmail.com>
2024-01-02 22:11:19 +0100

    dynamic routes

---
+++ 6_DynamicRoutesAndAdvancedModels/controllers/shop.js
@@ -0,0 +1,60 @@
+const Product = require('../models/product');
+
+//Much easier with async/await
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All products', 
+            path: '/products'
+        });
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        res.render('shop/product-detail', {
+            product: product, 
+            pageTitle: product.title,
+            path: '/products'
+        });
+    });
+}
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+        });
+    });
+};
+
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your cart'
+    });
+};
+
+exports.postCart = (req, res, next) => {
+    const productId = req.body.productId;
+    console.log(productId);
+    res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your orders'
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
+};
