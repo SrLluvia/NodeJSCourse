3712904ffa000eb38ee33eb4054eab1eac72da99
LanderLluv <landergll99@gmail.com>
2024-01-06 11:58:22 +0100

    sql intro

---
+++ 7_SQLIntro/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+//Creates express app
+const app = express();
+//Allows to set any values globally
+app.set('view engine', 'ejs');
+//Default
+app.set('views', 'views');
+//Allows to parse body
+app.use(bodyParser.urlencoded({extended: false}));
+//Serves static files (CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(6006);
