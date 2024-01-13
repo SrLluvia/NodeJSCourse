4d8b39721ae7cfbbdb39002d9bdbff0c908ee496
LanderLluv <landergll99@gmail.com>
2024-01-13 20:03:26 +0100

    cart and orders

--- 8_Sequelize/controllers/shop.js
+++ 8_Sequelize/controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.findAll().then(products => {
@@ -53,25 +52,89 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const productId = req.body.productId;
-    Product.findById(productId, (product) =>{
-        Cart.addProduct(productId, product.price);
-    });
-    res.redirect('/cart');
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user.getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts({where: {id: productId}});
+    })
+    .then(products => {
+        let product;
+        if(products.length > 0){
+            product = products[0];
+        }
+        
+        if(product){
+            const oldQuantity = product.cartItem.quantity;
+            newQuantity = oldQuantity + 1;
+            return product;
+        }
+        return Product.findByPk(productId);
+    })
+    .then(product => {
+        return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
+    })
+    .then(() => {
+        res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.cartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
+    req.user.getCart().then(cart => {
+        return cart.getProducts({where: {id: prodId}});
+    })
+    .then(products => {
+        const product = products[0];
+        //Deletes from the NM table only, not the product itself
+        return product.cartItem.destroy();
+    })
+    .then(result => {
         res.redirect('/cart');
-    });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your orders'
-    });
+    //Get all orders and all products related
+    req.user.getOrders({include: ['products']})
+    .then(orders => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your orders',
+            orders: orders
+        });
+    })
+    .catch(err => console.log(err));
+    
+};
+
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user.getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts();
+    })
+    .then(products => {
+        return req.user.createOrder()
+        .then(order => {
+            order.addProducts(products.map(product => {
+                product.orderItem = {quantity: product.cartItem.quantity};
+                return product;
+            }))
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result =>{
+        return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+        res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
--- 8_Sequelize/server.js
+++ 8_Sequelize/server.js
@@ -8,6 +8,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 //Creates express app
 const app = express();
@@ -40,13 +42,22 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 //Associations (in both directions)
+//User - Product
 User.hasMany(Product);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+//User - Cart
 User.hasOne(Cart);
 Cart.belongsTo(User);
+//Cart - Product
 //Creates NM table
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+//User - Order
+Order.belongsTo(User);
+User.hasMany(Order);
+//Order - Product
+Order.belongsToMany(Product, {through: OrderItem});
+
 
 //Creates a table for each model
 //force: true only in dev only to set up relations, not prod
@@ -66,12 +77,14 @@ sequelize
     })
     .then(user => {
         //console.log(user);
-        var cart = user.getCart();
-        if (!cart) {
-            // only create if no cart exists
-            return localUser.createCart();
-        }
-        return cart; 
+        return user.getCart().then(cart => {
+            if (!cart) {
+                // only create if no cart exists
+                return user.createCart();
+            }
+            return cart;
+        });
+         
     })
     .then(cart => {
         app.listen(6006);
