444f52b6fc4c84406be0ef493096b9ab84a90468
LanderLluv <landergll99@gmail.com>
2024-04-16 08:10:45 +0200

    base emails

---
+++ 13_Mails/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = 'mongodb+srv://admin:admin@cluster0.j8rsx4b.mongodb.net/shop'
+
+//Creates express app
+const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
+
+const csrfProtection = csrf();
+
+//Allows to set any values globally
+app.set('view engine', 'ejs');
+//Default
+app.set('views', 'views');
+//Allows to parse body
+app.use(bodyParser.urlencoded({ extended: false }));
+//Serves static files (CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store
+}));
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+    //No user in the session
+    if(!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+    .then(user => {
+        req. user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    //Affects to all res
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect(MONGODB_URI)
+.then(result => {
+    app.listen(6006);
+}).catch(err => {
+    console.log(err);
+});
