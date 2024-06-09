f842751d4371859a82793ad287af222f3e848ad0
LanderLluv <landergll99@gmail.com>
2024-06-09 21:10:02 +0200

    websockets + fix

--- 24_Websockets/app.js
+++ 24_Websockets/app.js
@@ -1,16 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
-const path = require('path');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
-//For uploading images
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, 'images');
@@ -20,57 +20,55 @@ const storage = multer.diskStorage({
     }
 });
 
-//For uploading images
 const fileFilter = (req, file, cb) => {
-    if(
-        file.mimetype === 'image/png' || 
-        file.mimetype === 'image/jpg' || 
-        file.mimetype === 'image/jpeg'
-    ) {
-        cb(null, true);
-    }else{
-        cb(null, false);
-    }
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
-//We expect to receive request in json format
-app.use(bodyParser.json());
-
-//For uploading images
-app.use(multer({storage: storage, fileFilter: fileFilter}).single('image'));
-//Serves images folder statically
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-//Add headers, BEFORE forwarding!
-//This avoids CORS(Cross-Origin Resource Sharing) errors
 app.use((req, res, next) => {
-    //Allow access to any client
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-//Forward requests that start with /feed
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
-//Middleware for error hanling
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({message: message, data: data});
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
-})
-
-mongoose.connect(
+mongoose
+  .connect(
     'mongodb+srv://admin:admin@cluster0.j8rsx4b.mongodb.net/backendjscourse'
-).then( result => {
-    app.listen(8080);
-}).catch(err => console.log(err));
-
-/*
-
-*/
+  )
+  .then(result => {
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
+  })
+  .catch(err => console.log(err));
--- 24_Websockets/routes/feed.js
+++ 24_Websockets/routes/feed.js
@@ -1,36 +1,43 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-//GET /feed/posts
+// GET /feed/posts
 router.get('/posts', isAuth, feedController.getPosts);
+
+// POST /feed/post
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
 router.get('/post/:postId', isAuth, feedController.getPost);
 
-//POST /feed/post
-router.post(
-    '/post',
-    isAuth,  
-    [
-        //Middleware to validate fields
-        body('title').trim().isLength({min: 5}),
-        body('content').trim().isLength({min: 5})
-    ],
-    feedController.postPost
-);
-
-router.put('/post/:postId',
-    isAuth,
-    [
-        //Middleware to validate fields
-        body('title').trim().isLength({min: 5}),
-        body('content').trim().isLength({min: 5})
-    ],
-    feedController.updatePost
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
 );
 
 router.delete('/post/:postId', isAuth, feedController.deletePost);
